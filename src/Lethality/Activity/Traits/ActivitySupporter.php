3aea29cd567cc77d95cd912def11f977f4315da2
Lee Holmes <lee@lethality.me.uk>
2018-02-11 01:20:34 +0000

    Rename

---
+++ src/Lethality/Activity/Traits/ActivitySupporter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lethality\Activity\Traits;
+
+trait ActivitySupporter
+{
+    use ActivitySorter, GuestRetriever, UserRetriever;
+
+    /**
+     * BelongsTo relationship with the user model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(config('auth.providers.users.model'));
+    }
+}
